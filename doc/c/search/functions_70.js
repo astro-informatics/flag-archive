90b741f94b677bfbcb7fc4e4593e074d54a7e588
Jason McEwen <jason.mcewen@ucl.ac.uk>
2012-11-09 11:02:04 +0000

    Added c documentation.

---
+++ doc/c/search/functions_70.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['print_5ff',['print_f',['../flag__fbtest_8c.html#aebb28c99408e287db25bc7d8e39f20f8',1,'print_f(const complex double *f, int L, int N):&#160;flag_fbtest.c'],['../flag__test_8c.html#aebb28c99408e287db25bc7d8e39f20f8',1,'print_f(const complex double *f, int L, int N):&#160;flag_test.c']]],
+  ['print_5ff_5freal',['print_f_real',['../flag__fbtest_8c.html#ae925420e0fb5f8dadb33f0838520b89c',1,'print_f_real(const double *f, int L, int N):&#160;flag_fbtest.c'],['../flag__test_8c.html#ae925420e0fb5f8dadb33f0838520b89c',1,'print_f_real(const double *f, int L, int N):&#160;flag_test.c']]],
+  ['print_5fflmn',['print_flmn',['../flag__fbtest_8c.html#a54b1181d3cf6764c5e10ebc6e8f85235',1,'print_flmn(const complex double *flmn, int L, int N):&#160;flag_fbtest.c'],['../flag__test_8c.html#a54b1181d3cf6764c5e10ebc6e8f85235',1,'print_flmn(const complex double *flmn, int L, int N):&#160;flag_test.c']]]
+];
---
+++ doc/c/search/functions_72.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['ran2_5fdp',['ran2_dp',['../flag__fbtest_8c.html#a8f1de4509d2d559e1cf392353eae7afa',1,'ran2_dp(int idum):&#160;flag_fbtest.c'],['../flag__test_8c.html#a8f1de4509d2d559e1cf392353eae7afa',1,'ran2_dp(int idum):&#160;flag_test.c']]]
+];
