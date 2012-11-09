5ecb3ae369a44d0a62736dbf2381ede0a2885b68
Jason McEwen <jason.mcewen@ucl.ac.uk>
2012-11-09 11:03:24 +0000

    Added c documentation to trunk.

---
+++ doc/c/search/files_66.js
@@ -0,0 +1,15 @@
+var searchData=
+[
+  ['flag_2eh',['flag.h',['../flag_8h.html',1,'']]],
+  ['flag_5fabout_2ec',['flag_about.c',['../flag__about_8c.html',1,'']]],
+  ['flag_5fcore_2ec',['flag_core.c',['../flag__core_8c.html',1,'']]],
+  ['flag_5fcore_2eh',['flag_core.h',['../flag__core_8h.html',1,'']]],
+  ['flag_5ffbtest_2ec',['flag_fbtest.c',['../flag__fbtest_8c.html',1,'']]],
+  ['flag_5fsampling_2ec',['flag_sampling.c',['../flag__sampling_8c.html',1,'']]],
+  ['flag_5fsampling_2eh',['flag_sampling.h',['../flag__sampling_8h.html',1,'']]],
+  ['flag_5fspherbessel_2ec',['flag_spherbessel.c',['../flag__spherbessel_8c.html',1,'']]],
+  ['flag_5fspherbessel_2eh',['flag_spherbessel.h',['../flag__spherbessel_8h.html',1,'']]],
+  ['flag_5fspherlaguerre_2ec',['flag_spherlaguerre.c',['../flag__spherlaguerre_8c.html',1,'']]],
+  ['flag_5fspherlaguerre_2eh',['flag_spherlaguerre.h',['../flag__spherlaguerre_8h.html',1,'']]],
+  ['flag_5ftest_2ec',['flag_test.c',['../flag__test_8c.html',1,'']]]
+];
---
+++ doc/c/search/functions_61.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['allocate_5fssht_5fsampling',['allocate_ssht_sampling',['../flag__sampling_8c.html#a75998f2c95b3b1775c9a9ea6c699ec0a',1,'flag_sampling.c']]]
+];
