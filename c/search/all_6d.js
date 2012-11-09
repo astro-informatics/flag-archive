bf262d248a94eaad3b4340de57a581f20c428415
Jason McEwen <jason.mcewen@ucl.ac.uk>
2012-11-09 11:03:24 +0000

    Added c documentation to trunk.

---
+++ c/search/all_6d.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['main',['main',['../flag__about_8c.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;flag_about.c'],['../flag__fbtest_8c.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;flag_fbtest.c'],['../flag__test_8c.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;flag_test.c']]],
+  ['max',['MAX',['../flag__fbtest_8c.html#afa99ec4acc4ecb2dc3c2d05da15d0e3f',1,'MAX():&#160;flag_fbtest.c'],['../flag__test_8c.html#afa99ec4acc4ecb2dc3c2d05da15d0e3f',1,'MAX():&#160;flag_test.c']]],
+  ['maxerr',['maxerr',['../flag__fbtest_8c.html#a4f5998b7b92ffac7fea15fee7ce645f0',1,'maxerr(double *a, double *b, int size):&#160;flag_fbtest.c'],['../flag__test_8c.html#a4f5998b7b92ffac7fea15fee7ce645f0',1,'maxerr(double *a, double *b, int size):&#160;flag_test.c']]],
+  ['maxerr_5fcplx',['maxerr_cplx',['../flag__fbtest_8c.html#a393b1deb6be62de0afa639a203b78b15',1,'maxerr_cplx(complex double *a, complex double *b, int size):&#160;flag_fbtest.c'],['../flag__test_8c.html#a393b1deb6be62de0afa639a203b78b15',1,'maxerr_cplx(complex double *a, complex double *b, int size):&#160;flag_test.c']]],
+  ['min',['MIN',['../flag__spherlaguerre_8c.html#a3acffbd305ee72dcd4593c0d8af64a4f',1,'flag_spherlaguerre.c']]]
+];
---
+++ c/search/functions_73.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['ssht_5fflm_5fsize',['ssht_flm_size',['../flag__core_8c.html#a003121e3f4413c5c3d6a9d45c436216c',1,'flag_core.c']]],
+  ['ssht_5ffr_5fsize_5fmw',['ssht_fr_size_mw',['../flag__core_8c.html#a0f424c05ca0f94faa2cb1f8903329904',1,'flag_core.c']]],
+  ['ssht_5fsampling_5fmw',['ssht_sampling_mw',['../flag__sampling_8c.html#a4b16cb844e2194bf82e2a341a30b1add',1,'flag_sampling.c']]]
+];
