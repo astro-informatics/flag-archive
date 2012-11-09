39fdd126d983d6084ada653f0c3c024ff869ad93
Jason McEwen <jason.mcewen@ucl.ac.uk>
2012-11-09 11:02:04 +0000

    Added c documentation.

---
+++ c/search/all_6a.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['j_5fell',['j_ell',['../flag__spherbessel_8c.html#af0797310df815602c3294bfc9c8d7390',1,'j_ell(double X, int l):&#160;flag_spherbessel.c'],['../flag__spherbessel_8h.html#af0797310df815602c3294bfc9c8d7390',1,'j_ell(double X, int l):&#160;flag_spherbessel.c']]]
+];
---
+++ c/search/functions_6d.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['main',['main',['../flag__about_8c.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;flag_about.c'],['../flag__fbtest_8c.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;flag_fbtest.c'],['../flag__test_8c.html#a0ddf1224851353fc92bfbff6f499fa97',1,'main(int argc, char *argv[]):&#160;flag_test.c']]],
+  ['maxerr',['maxerr',['../flag__fbtest_8c.html#a4f5998b7b92ffac7fea15fee7ce645f0',1,'maxerr(double *a, double *b, int size):&#160;flag_fbtest.c'],['../flag__test_8c.html#a4f5998b7b92ffac7fea15fee7ce645f0',1,'maxerr(double *a, double *b, int size):&#160;flag_test.c']]],
+  ['maxerr_5fcplx',['maxerr_cplx',['../flag__fbtest_8c.html#a393b1deb6be62de0afa639a203b78b15',1,'maxerr_cplx(complex double *a, complex double *b, int size):&#160;flag_fbtest.c'],['../flag__test_8c.html#a393b1deb6be62de0afa639a203b78b15',1,'maxerr_cplx(complex double *a, complex double *b, int size):&#160;flag_test.c']]]
+];
